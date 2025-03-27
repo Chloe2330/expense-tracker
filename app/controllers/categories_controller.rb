400322e6d4aa8785d265598abd3b23228d281b2b
Chloe2330 <chloe.lin1577@gmail.com>
2025-03-26 23:44:49 -0400

    feat: Categories (update/delete) + style changes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,10 @@
 class CategoriesController < ApplicationController
-    before_action :set_category, only: %i[ show ]
+    before_action :set_category, only: %i[ show edit update destroy ]
 
+    def index
+      @categories = Category.all
+    end
+    
     def show
     end
 
@@ -15,6 +19,26 @@ class CategoriesController < ApplicationController
         else
           render :new, status: :unprocessable_entity
         end
+    end
+
+    def edit
+    end
+  
+    def update
+      if @category.update(category_params)
+        redirect_to @category
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      begin
+        @category.destroy
+        redirect_to categories_path, notice: 'Category was successfully deleted.'
+      rescue ActiveRecord::InvalidForeignKey
+        redirect_to categories_path, alert: 'Category cannot be deleted because it is referenced by an expense.'
+      end
     end
 
     private
